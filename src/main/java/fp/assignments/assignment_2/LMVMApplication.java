3b9af20ff1625a6b70c3bb69ce6083c90cdbee62
Raf <81792786+kinda-raffy@users.noreply.github.com>
2025-02-01 23:40:56 +1100

    refactor: entry rename

--- src/main/java/fp/assignments/assignment_2/LMVMApplication.java
+++ src/main/java/fp/assignments/assignment_2/LMVMApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class LMVMApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(LMVMApplication.class.getResource("views/main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
         stage.setTitle("Live Music Venue Matchmaker");
         stage.setScene(scene);
