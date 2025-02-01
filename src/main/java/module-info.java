1fa79215b356722ea4ea528e94480443f514176e
Raf <81792786+kinda-raffy@users.noreply.github.com>
2025-02-01 14:53:45 +1100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module fp.assignments.assignment_2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens fp.assignments.assignment_2 to javafx.fxml;
+    exports fp.assignments.assignment_2;
+}
