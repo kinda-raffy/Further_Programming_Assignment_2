6c4a07c00b6fad038b110630e9eaa60424002697
Raf <81792786+kinda-raffy@users.noreply.github.com>
2025-02-04 20:56:18 +1100

    feat: user account creation

--- src/main/java/fp/assignments/assignment_2/controller/AddEmployeeFormController.java
+++ src/main/java/fp/assignments/assignment_2/controller/AddEmployeeFormController.java
@@ -1,5 +1,70 @@
 package fp.assignments.assignment_2.controller;
 
-public class AddEmployeeFormController extends BaseController {
+import fp.assignments.assignment_2.service.DatabaseConnection;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import java.sql.SQLException;
 
+public class AddEmployeeFormController {
+  @FXML
+  private TextField userNameField;
+  @FXML
+  private PasswordField passwordField;
+  @FXML
+  private TextField firstNameField;
+  @FXML
+  private TextField lastNameField;
+  @FXML
+  private ComboBox<String> typeComboBox;
+  @FXML
+  private Label errorLabel;
+
+  @FXML
+  public void initialize() {
+    typeComboBox.getItems().addAll("manager", "staff");
+    typeComboBox.setValue("staff");
+  }
+
+  @FXML
+  private void handleSubmit() {
+    if (validateFields()) {
+      try {
+        String sql = """
+            INSERT INTO users (user_name, password, first_name, last_name, type)
+            VALUES (?, ?, ?, ?, ?)
+            """;
+
+        DatabaseConnection.getInstance().executeUpdate(sql, ps -> {
+          ps.setString(1, userNameField.getText());
+          ps.setString(2, passwordField.getText());
+          ps.setString(3, firstNameField.getText());
+          ps.setString(4, lastNameField.getText());
+          ps.setString(5, typeComboBox.getValue());
+        });
+
+        closeWindow();
+      } catch (SQLException e) {
+        errorLabel.setText("Error creating user: " + e.getMessage());
+      }
+    }
+  }
+
+  private boolean validateFields() {
+    if (userNameField.getText().isEmpty() || passwordField.getText().isEmpty() ||
+        firstNameField.getText().isEmpty() || lastNameField.getText().isEmpty()) {
+      errorLabel.setText("All fields are required");
+      return false;
+    }
+    return true;
+  }
+
+  @FXML
+  private void handleCancel() {
+    closeWindow();
+  }
+
+  private void closeWindow() {
+    ((Stage) userNameField.getScene().getWindow()).close();
+  }
 }
--- src/main/java/fp/assignments/assignment_2/controller/EmployeeController.java
+++ src/main/java/fp/assignments/assignment_2/controller/EmployeeController.java
@@ -1,5 +1,24 @@
 package fp.assignments.assignment_2.controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 public class EmployeeController extends BaseController {
 
+  public void showAddEmployeeForm() {
+    try {
+      FXMLLoader loader = new FXMLLoader(
+          getClass().getResource("/fp/assignments/assignment_2/view/add-employee-form-view.fxml"));
+      Stage stage = new Stage();
+      stage.initModality(Modality.APPLICATION_MODAL);
+      stage.setTitle("Add Staff Member");
+      stage.setScene(new Scene(loader.load(), 500, 600));
+      stage.showAndWait();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
