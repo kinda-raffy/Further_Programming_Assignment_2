2d2684045b28588f74e22e8245a95a60717ca0a2
Raf <81792786+kinda-raffy@users.noreply.github.com>
2025-02-06 21:30:26 +1100

    refactor: extend base controller alerts

--- src/main/java/fp/assignments/assignment_2/controller/BaseController.java
+++ src/main/java/fp/assignments/assignment_2/controller/BaseController.java
@@ -4,8 +4,28 @@ import javafx.scene.control.Alert;
 
 public abstract class BaseController {
   protected void showError(String header, String content) {
-    Alert alert = new Alert(Alert.AlertType.ERROR);
-    alert.setTitle("Error");
+    showAlert(Alert.AlertType.ERROR, header, content);
+  }
+
+  protected void showSuccess(String header, String content) {
+    showAlert(Alert.AlertType.INFORMATION, header, content);
+  }
+
+  protected void showConfirmation(String header, String content, Runnable onConfirm) {
+    showAlert(Alert.AlertType.CONFIRMATION, header, content);
+  }
+
+  protected void showWarning(String header, String content) {
+    showAlert(Alert.AlertType.WARNING, header, content);
+  }
+
+  protected void showInfo(String header, String content) {
+    showAlert(Alert.AlertType.INFORMATION, header, content);
+  }
+
+  private void showAlert(Alert.AlertType type, String header, String content) {
+    Alert alert = new Alert(type);
+    alert.setTitle(header);
     alert.setHeaderText(header);
     alert.setContentText(content);
     alert.showAndWait();
---
+++ src/main/java/fp/assignments/assignment_2/service/UserService.java
@@ -0,0 +1,5 @@
+package fp.assignments.assignment_2.service;
+
+public class UserService {
+
+}
